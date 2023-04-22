009deb7c4c0b589e34373ee4e7768df9ad1b23f7
Shane <gupitshanehazel@gmail.com>
2023-04-22 21:29:25 +0800

    pseudocode

---
+++ multiple commit 2.py
@@ -0,0 +1,12 @@
+# pseudocode
+
+# ask user for plaintext and keyword (all caps, no spaces)
+# define cipher string
+# make the plaintext and keyword all caps and no spaces
+# store the value in cipher code
+# check every character in the plaintext
+# translate letter to its corresponding nuumber
+# encrypt the plaintext with the given keyword
+# translate number to its corresponding letter
+# convert cipher text int to str
+# print output
